10e4705506305f3d5c8d2c361ac98c049b1c2c4b
Eliza Jeyakumar <ejeyakumar@equitable.ca>
2020-10-29 21:22:17 -0400

    Add API

--- src/app/models/interested-person.ts
+++
@@ -1,9 +0,0 @@
-import { ActivityTypes } from './activity-types';
-
-export class InterestedPerson {
-    firstName: string;
-    lastName: string;
-    email: string;
-    activityTypeId: number;
-    comments: string;
-};
